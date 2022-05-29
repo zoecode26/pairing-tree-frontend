34478bb96ef822bd333478b0a24d56b8fa61b758
Zoe Thomas <zoethomas1540@gmail.com>
2022-05-29 12:58:47 +0100

    Change axios location to point to heroku backend

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/'
+  baseURL: 'https://pair-tree-backend.herokuapp.com/'
 });
-
-//local server url
-// baseURL: 'http://localhost:3000/',
-// heroku url: 'https://pacific-brook-60350.herokuapp.com/'
 
 export default instance;
